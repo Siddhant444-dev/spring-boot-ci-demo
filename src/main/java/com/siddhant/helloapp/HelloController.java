270b7f38056dc5508cb5fb8f3b128fca40099d50
Siddhant444-dev <sidsinha491@gmail.com>
2026-06-22 08:06:41 +0530

    update

--- src/main/java/com/siddhant/helloapp/HelloController.java
+++ src/main/java/com/siddhant/helloapp/HelloController.java
@@ -1,4 +1,13 @@
 package com.siddhant.helloapp;
 
-public class Controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello from CI:CD Pipeline...";
+    }
 }
